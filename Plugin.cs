a8216ff1cbbfa2e12736a521f56a0cf015f10e68
Xapu1337 <xapursplaz@gmail.com>
2025-03-26 21:05:09 +0100

    add missing files

---
+++ Plugin.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+
+namespace JustRetryPlus
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal new static ManualLogSource Logger = null!;
+
+        private static ConfigEntry<bool> _shouldResetLevel = null!;
+        public static bool ShouldResetLevel => _shouldResetLevel.Value;
+        
+        private static ConfigEntry<bool> _shouldHealAfterFail = null!;
+        public static bool ShouldHealAfterFail => _shouldHealAfterFail.Value;
+
+        public static bool DidFailLevel { get; set; } = false;
+
+        private static ConfigEntry<bool> _isEnabled = null!;
+        public static bool IsEnabled => _isEnabled.Value;
+        
+        private static ConfigEntry<int> _healthToHeal = null!;
+        public static int HealthToHealValue => _healthToHeal.Value;
+        private void Awake()
+        {
+            Logger = base.Logger;
+            Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+            Logger.LogInfo($"Thanks for nickklmao for the original Idea and bits of code! please check out their mods on Thunderstore!");
+            Logger.LogInfo($"Without them this mod wouldn't be possible!");
+           
+            var harmony = new Harmony("eu.xaru.justretryplus");
+            harmony.PatchAll();
+            Logger.LogInfo("Harmony patches applied.");
+            
+            _isEnabled = Config.Bind("Settings", "IsEnabled", true,
+                new ConfigDescription("Toggle if the plugin should be enabled, if false the normal game death scene will take over", new AcceptableValueRange<bool>(false, true)));
+            
+            _shouldResetLevel = Config.Bind("Settings", "ShouldResetLevel", false,
+                new ConfigDescription("Toggle if the game should restart at level one & reset the map when failing a level", new AcceptableValueRange<bool>(false, true)));
+
+            _shouldHealAfterFail = Config.Bind("Settings", "ShouldHealAfterFail", true,
+                new ConfigDescription("Toggle if the game should restart with full health when failing a level", new AcceptableValueRange<bool>(false, true)));
+            
+            _healthToHeal = Config.Bind("Settings", "HealthToHeal", 100,
+                new ConfigDescription("The amount of health to heal when failing a level", new AcceptableValueRange<int>(0, 500)));
+        }
+
+        private void Start() => Logger.LogInfo("JustRetryPlus started!");
+     
+    }
+
+    [HarmonyPatch(typeof(RunManager), "ChangeLevel")]
+    public static class ChangeLevelPatch
+    {
+        static void Prefix(
+            RunManager __instance, 
+            ref bool _completedLevel,
+            ref bool _levelFailed,
+            ref RunManager.ChangeLevelType _changeLevelType)
+        {
+            if(!Plugin.IsEnabled) return;
+            Plugin.Logger.LogInfo($"Level failed?: {_levelFailed}");
+            Plugin.DidFailLevel = _levelFailed;
+            if(!Plugin.DidFailLevel) return; 
+            if(Plugin.ShouldResetLevel)
+            {
+                Plugin.Logger.LogInfo("Restarting at level one");
+                __instance.ResetProgress(); // reset progress
+                __instance.SetRunLevel(); // rotate levels
+            }
+
+            Plugin.Logger.LogInfo("Level failed, retrying...");
+            __instance.RestartScene();
+            Plugin.Logger.LogInfo("Restarted scene");
+        }
+        
+    }
+    
+    [HarmonyPatch(typeof(RoundDirector), "StartRoundLogic")]
+    public static class RoundDirectorPatch
+    {
+        static void Postfix(RoundDirector __instance)
+        {
+            if(!Plugin.IsEnabled) return;
+            if (!Plugin.ShouldHealAfterFail || !Plugin.DidFailLevel || !SemiFunc.IsMasterClientOrSingleplayer()) return;
+            foreach (var item in from player in SemiFunc.PlayerGetAll() select player.playerHealth)
+            {
+                if (item == null) continue;
+                item.HealOther(Plugin.HealthToHealValue, true);
+            }
+        }
+        
+        
+    }
+    
+}
---
+++ readme.md
@@ -0,0 +1,32 @@
+﻿## JustRetryPlus
+
+Based on JustRetry from https://thunderstore.io/c/repo/p/nickklmao/JustRetry/
+
+this mod skips the round end and heals all players to full health when retrying a level.
+
+## Changelog
+26.03.2025 - Added a more stable, non wait based healing that fires on the round start
+I couldn't at first find it but nickklmao had already implemented a round start event that I could use to heal the players. 
+
+## Building and finding the .dlls
+1. Clone the repository
+2. Open the solution in Visual Studio, Rider or VSCode
+3. Go to Steam, find R.E.P.O, right click, properties, local files, browse local files
+4. go to the REPO_Data\Managed folder
+5. COPY the following dlls to the JustRetryPlus/libs folder:
+    - Assembly-CSharp.dll < required, the others are optional but may be of use when modifiying this mod)
+    - Facepunch.Steamworks.Win64.dll < optional >
+    - Newtonsoft.Json.dll < optional >
+    - Photon3Unity3D.dll < optional >
+    - PhotonChat.dll < optional >
+    - PhotonRealtime.dll < optional >
+    - PhotonUnityNetworking.dll < optional >
+    - PhotonUnityNetworking.Utilities.dll < optional >
+    - PhotonVoice.dll < optional >
+    - PhotonVoice.API.dll < optional >
+    - PhotonVoice.PUN.dll < optional >
+    - Unity.TextMeshPro.dll < optional >
+    - UnityEngine.UI.dll < optional >
+6. Build the solution
+7. Go to Thunderstore, Select R.E.P.O, go to a profile, "Settings" and "Import local mod"
+8. Select the .dll from the JustRetryPlus/bin/Debug folder
